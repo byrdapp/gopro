14280bab8be51c25282ebe16999893fc621f9d74
blixenkrone <sblixenkrone@gmail.com>
2020-01-09 13:53:31 +0100

    Update exif.go

--- pkg/exif/exif.go
+++ pkg/exif/exif.go
@@ -1,6 +1,9 @@
 package exif
 
 import (
+	"github.com/pkg/errors"
+	goexif "github.com/rwcarlsen/goexif/exif"
+
 	"github.com/blixenkrone/gopro/pkg/logger"
 )
 
@@ -17,16 +20,27 @@ type Output struct {
 	PixelXDimension int               `json:"pixelXDimension,omitempty"`
 	PixelYDimension int               `json:"pixelYDimension,omitempty"`
 	MediaSize       float64           `json:"mediaSize,omitempty"`
-	ExifErrors      map[string]string `json:"errors,omitempty"`
+	MissingExif     map[string]string `json:"missingExif,omitempty"`
 	// MediaFormat     string  `json:"mediaFormat,omitempty"`
 }
 
 var log = logger.NewLogger()
 
 // adds an object to the output JSON that displays missing exif data
-func (o *Output) MissingExif(errType string, err error) {
-	log.Errorf("exif added msg: %s from type: %s", err, errType)
-	o.ExifErrors[errType] = err.Error()
+func (o *Output) AddMissingExif(errType string, originError error) {
+	var returnError error
+	if goexif.IsTagNotPresentError(originError) {
+		returnError = errors.Wrapf(originError, "exif missing from type: %s", errType)
+	}
+	if goexif.IsCriticalError(originError) {
+		returnError = errors.Wrapf(originError, "critical error with %s", errType)
+	}
+	if goexif.IsGPSError(originError) {
+		returnError = errors.Wrapf(originError, "geographical error with %s", errType)
+	}
+	log.Errorf("Origin error: %s - Client error: %s - Type?: %s", originError, returnError, errType)
+	returnError = errors.Errorf("error parsing from type %s", errType)
+	o.MissingExif[errType] = returnError.Error()
 }
 
 func (o *Output) MediaType() (mediaType *string) {
